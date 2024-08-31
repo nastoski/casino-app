4f53750268cb11e411341a530d96556e604c2807
Hristijan Nastoski <nastoskih@gmail.com>
2024-08-31 18:53:42 +0200

    clear css files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import React from 'react';
+import './globals.css';
 import Layout from './components/Layout';
 import { store } from '@/store/store';
 import { Provider } from 'react-redux';
-import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
